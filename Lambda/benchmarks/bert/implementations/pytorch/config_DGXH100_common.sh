b5f8a798754b409ae515c2fb00671d4250f53c09
chuanli11 <cl.chuanli@gmail.com>
2024-07-10 06:04:06 +0000

    Use ml-512-head-001 for self-hosted registery

--- Lambda/benchmarks/bert/implementations/pytorch/config_DGXH100_common.sh
+++ Lambda/benchmarks/bert/implementations/pytorch/config_DGXH100_common.sh
@@ -4,5 +4,5 @@ export DGXSOCKETCORES=64
 export DGXNSOCKET=2
 export DGXHT=2         # HT is on is 2, HT off is 1
 export SLURM_NTASKS=${DGXNGPU}
-export CONT="172.26.134.239:5000#local/mlperf-nvidia-24.05:language_model"
+export CONT="$(hostname):5000#local/mlperf-nvidia-24.05:language_model"
 export NEXP=1
